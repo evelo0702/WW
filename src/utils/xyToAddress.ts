4cdaf9159b44006b1bfc60ad224a4c4ff920a96e
EVELO <evelo0702@gmail.com>
2024-08-05 15:12:36 +0900

    feat : 주소값으로 위도 경도 구하는 함수

---
+++ src/utils/xyToAddress.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+const KAKAO_RESTAPI_KEY = import.meta.env.VITE_KAKAO_RESTAPI_KEY;
+import { totalAddress } from "../components/SearchModal";
+export const xyToAddress = async (address: totalAddress) => {
+  const url = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${address.lng}&y=${address.lat}`;
+
+  const res = await axios.get(url, {
+    headers: {
+      Authorization: `KakaoAK ${KAKAO_RESTAPI_KEY}`,
+    },
+  });
+  let temp = {
+    ...address,
+    regionName:
+      res.data.documents[1].region_2depth_name +
+      " " +
+      res.data.documents[1].region_3depth_name,
+  };
+  return temp;
+};
