e77e6219e17a76d0b22f3db704770653d25929c2
EVELO <evelo0702@gmail.com>
2024-08-15 21:10:58 +0900

    feat : ai로 받아온 데이터를 보여주는 컴포넌트

--- src/components/RecWear.tsx
+++ src/components/RecWear.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import GptChat from "./GptChat";
+import { changeGptComment } from "../utils/changeGptComment";
 
-const RecWear = () => {
+interface Props {
+  summary: string;
+}
+const RecWear: React.FC<Props> = ({ summary }) => {
+  const [comment, setComment] = useState("");
+  const [ansGpt, setAns] = useState<string[]>([]);
+  useEffect(() => {
+    if (comment) {
+      let temp = changeGptComment(comment);
+      setAns(temp);
+    }
+  }, [comment]);
+
   return (
-    <div className="bg-sky-400  p-5 md:h-screen md:w-1/3 w-full h-2/5 flex flex-col">
-      <div className="bg-red-400 mb-10 md:h-1/4 h-1/4 md:mb-10">코멘트</div>
-      <div className="max-[768px]:flex md:h-full flex-grow ">
-        <div className="bg-teal-400 md:h-1/5 w-1/3 md:w-full">모자</div>
-        <div className="bg-yellow-100 md:h-2/5 w-1/3 md:w-full">상의</div>
-        <div className="bg-green-400 md:h-2/5 w-1/3 md:w-full">하의</div>
+    <div className="h-full w-full flex flex-col max-[460px]:text-sm">
+      <p className="text-4xl text-center my-4">오늘의 날씨 from AI </p>
+      {summary && <GptChat summary={summary} setComment={setComment} />}
+
+      <div className="md:h-1/4 h-1/4 flex items-center p-4 border-y-2">
+        {ansGpt[0] && <p> 날씨 요약: {ansGpt[0]}</p>}
+      </div>
+      <div className="h-1/4 flex items-center p-4 ">
+        {ansGpt[1] && <p> 날씨에 맞는 의상: {ansGpt[1]}</p>}
+      </div>
+      <div className="max-[768px]:flex md:h-1/4 flex-grow flex border-2 rounded-md p-4 items-center">
+        <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
+          {ansGpt[2] &&
+            ansGpt[2].split(",").map((i, index) => <div key={index}>{i}</div>)}
+        </div>
+        <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
+          {ansGpt[3] &&
+            ansGpt[3].split(",").map((i, index) => <div key={index}>{i}</div>)}
+        </div>
       </div>
     </div>
   );
