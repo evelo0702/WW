d422ebd7d6d242be756e1e44594637cabfbda02b
EVELO <evelo0702@gmail.com>
2024-08-16 14:57:22 +0900

    feat: getIcon으로 주간날씨 아이콘 받아오는 메소드 추가

--- src/utils/getIcon.ts
+++ src/utils/getIcon.ts
@@ -1,4 +1,4 @@
-import { todayWeather } from "../model/types";
+import { todayWeather, weekendWeatherData } from "../model/types";
 
 export const getIcon = (TodayWeather: todayWeather[]) => {
   // 1순위 - 강수형태 2순위 - 하늘 상태
@@ -20,15 +20,15 @@ export const getIcon = (TodayWeather: todayWeather[]) => {
             TodayWeather[i].ICON = "Rainy";
             break;
         }
-      } else if (parseInt(TodayWeather[i].SKY) <= 5) {
+      } else if (parseInt(TodayWeather[i].SKY) === 1) {
         if (parseInt(TodayWeather[i].TIME) < 2000) {
           TodayWeather[i].ICON = "Sunny";
         } else {
           TodayWeather[i].ICON = "Clear";
         }
       } else if (
-        5 < parseInt(TodayWeather[i].SKY) &&
-        parseInt(TodayWeather[i].SKY) <= 8
+        parseInt(TodayWeather[i].SKY) === 3 &&
+        parseInt(TodayWeather[i].TIME) < 2000
       ) {
         TodayWeather[i].ICON = "PartlyCloudy";
       } else {
@@ -37,3 +37,100 @@ export const getIcon = (TodayWeather: todayWeather[]) => {
     }
   }
 };
+
+export const getWeekendWeatherIcon = (
+  weekendWeatherData: weekendWeatherData
+) => {
+  let temp = { amIcon: "", pmIcon: "" };
+
+  switch (weekendWeatherData.wfAm) {
+    case "맑음":
+      temp.amIcon = "Sunny";
+      break;
+    case "구름많음":
+      temp.amIcon = "PartlyCloudy";
+      break;
+    case "구름 많음":
+      temp.amIcon = "PartlyCloudy";
+      break;
+    case "구름많고 비":
+      temp.amIcon = "Rainy";
+      break;
+    case "구름많고 눈":
+      temp.amIcon = "Snowy";
+      break;
+    case "구름많고 비/눈":
+      temp.amIcon = "Rainy";
+      break;
+    case "구름많고 소나기":
+      temp.amIcon = "Rainy";
+      break;
+    case "흐림":
+      temp.amIcon = "Cloudy";
+      break;
+    case "흐리고 비":
+      temp.amIcon = "Rainy";
+      break;
+    case "흐리고 비/눈":
+      temp.amIcon = "Sleet";
+      break;
+    case "흐리고 소나기":
+      temp.amIcon = "Rainy";
+      break;
+    case "흐리고 눈":
+      temp.amIcon = "Snowy";
+      break;
+  }
+  switch (weekendWeatherData.wfPm) {
+    case "맑음":
+      temp.pmIcon = "Clear";
+      break;
+    case "구름많음":
+      temp.pmIcon = "Cloudy";
+      break;
+    case "구름 많음":
+      temp.pmIcon = "Cloudy";
+      break;
+
+    case "구름많고 비":
+      temp.pmIcon = "Rainy";
+      break;
+    case "구름많고 눈":
+      temp.pmIcon = "Snowy";
+      break;
+    case "구름많고 비/눈":
+      temp.pmIcon = "Rainy";
+      break;
+    case "구름많고 소나기":
+      temp.pmIcon = "Rainy";
+      break;
+    case "흐림":
+      temp.pmIcon = "Cloudy";
+      break;
+    case "흐리고 비":
+      temp.pmIcon = "Rainy";
+      break;
+    case "흐리고 비/눈":
+      temp.pmIcon = "Sleet";
+      break;
+    case "흐리고 소나기":
+      temp.pmIcon = "Rainy";
+      break;
+    case "흐리고 눈":
+      temp.pmIcon = "Snowy";
+      break;
+  }
+  return temp;
+  //  맑음 - sunny(오전) / clear(오후)
+  // - 구름많음 - partlyCloudy, 구름많고 비 rainy , 구름많고 눈 snowy , 구름많고 비/눈 sleet, 구름많고 소나기
+  // - 흐림 - cloudy , 흐리고 비 rainy, 흐리고 눈 snowy, 흐리고 비/눈 sleet, 흐리고 소나기
+};
+// {
+//   "day": "1일후",
+//   "rnStAm": 0,
+//   "rnStPm": 60,
+//   "taMin": 26,
+//   "wfAm": "맑음",
+//   "taMax": 35,
+//   "wfPm": "구름 많음"
+// }
