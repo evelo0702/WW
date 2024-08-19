af166d92d93236e42d5775780ac12e9c51f973cc
EVELO <evelo0702@gmail.com>
2024-08-19 14:14:20 +0900

    Revert "update: gpt 호출 코멘트 변경4"
    This reverts commit 00307ca0550c573e4319759b2f3e5a9dee944252.

--- src/components/RecWear.tsx
+++ src/components/RecWear.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import GptChat from "./GptChat";
 import { changeGptComment } from "../utils/changeGptComment";
-import Loader from "./Loader";
 
 interface Props {
   summary: string;
@@ -13,7 +12,6 @@ const RecWear: React.FC<Props> = ({ summary, setGender, gender }) => {
   const [ansGpt, setAns] = useState<string[]>([]);
   useEffect(() => {
     if (comment && gender) {
-      setAns([]);
       let temp = changeGptComment(comment);
       setAns(temp);
     }
@@ -27,9 +25,7 @@ const RecWear: React.FC<Props> = ({ summary, setGender, gender }) => {
           className={`${
             gender === "남성" ? "bg-sky-300" : "bg-white"
           } rounded-md w-1/2`}
-          onClick={() => {
-            setGender("남성");
-          }}
+          onClick={() => setGender("남성")}
         >
           남성
         </button>
@@ -37,37 +33,30 @@ const RecWear: React.FC<Props> = ({ summary, setGender, gender }) => {
           className={`${
             gender === "여성" ? "bg-red-300" : "bg-white"
           } rounded-md w-1/2`}
-          onClick={() => {
-            setGender("여성");
-          }}
+          onClick={() => setGender("여성")}
         >
           여성
         </button>
       </div>
       {!gender && <div className="text-center">성별을 선택해주세요</div>}
       {summary && <GptChat summary={summary} setComment={setComment} />}
-      <>
-        <div className="md:h-1/4 h-1/4 flex items-center p-4 border-y-2">
-          {ansGpt[0] && <p> 날씨 요약: {ansGpt[0]}</p>}
+
+      <div className="md:h-1/4 h-1/4 flex items-center p-4 border-y-2">
+        {ansGpt[0] && <p> 날씨 요약: {ansGpt[0]}</p>}
+      </div>
+      <div className="h-1/4 flex items-center p-4 ">
+        {ansGpt[1] && <p> 날씨에 맞는 의상: {ansGpt[1]}</p>}
+      </div>
+      <div className="max-[768px]:flex md:h-1/4 flex-grow flex border-2 rounded-md p-4 items-center">
+        <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
+          {ansGpt[2] &&
+            ansGpt[2].split(",").map((i, index) => <div key={index}>{i}</div>)}
         </div>
-        <div className="h-1/4 flex items-center p-4 ">
-          {ansGpt[1] && <p> 날씨에 맞는 의상: {ansGpt[1]}</p>}
+        <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
+          {ansGpt[3] &&
+            ansGpt[3].split(",").map((i, index) => <div key={index}>{i}</div>)}
         </div>
-        <div className="max-[768px]:flex md:h-1/4 flex-grow flex border-2 rounded-md p-4 items-center">
-          <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
-            {ansGpt[2] &&
-              ansGpt[2]
-                .split(",")
-                .map((i, index) => <div key={index}>{i}</div>)}
-          </div>
-          <div className=" md:h-1/3 w-1/2 md:w-full text-center flex flex-col justify-center">
-            {ansGpt[3] &&
-              ansGpt[3]
-                .split(",")
-                .map((i, index) => <div key={index}>{i}</div>)}
-          </div>
-        </div>
-      </>
+      </div>
     </div>
   );
 };
